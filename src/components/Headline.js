7ff3f2e6b7888a99c489bc32a897cec5473aa10c
Adrian Brooks <adrian.brooks@magnolia-cms.com>
2022-01-20 10:08:40 +0100

    updated the headline component

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -4,6 +4,7 @@ const Headline = props =>
 
 <div> 
 <h2 className="Headline">{props.text}</h2>
+<h4 className="Headline">{props.title}</h4> 
 <h4 className="Headline">{props.subtitle}</h4> 
 </div>
 
